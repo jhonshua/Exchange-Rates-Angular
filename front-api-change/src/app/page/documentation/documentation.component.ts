5b120359830460ee08b6af81d05eef94510fcd88
Julio-PC\Julio <julio.llinas@gmail.com>
2024-06-19 20:25:59 -0400

    actualizacion

--- front-api-change/src/app/page/documentation/documentation.component.ts
+++ front-api-change/src/app/page/documentation/documentation.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-documentation',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './documentation.component.html',
   styleUrl: './documentation.component.css'
 })
